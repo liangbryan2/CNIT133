d4fdfb1195a1b7148a0429c6c834b91ed65fbfae
Bryan Liang <liangbryan2@gmail.com>
2020-01-25 12:14:56 -0800

    hw2 part 1 done

---
+++ hw2.js
@@ -0,0 +1,15 @@
+function changePart(element) {
+    let items = document.getElementsByClassName('part');
+    for(var i = 0; i < items.length; i++) {
+        items[i].style.display = "none";
+    }
+    document.getElementById(element).style.display = "block";
+}
+
+function active(element) {
+    let items = document.getElementsByClassName('hw2-nav');
+    for(var i = 0; i < items.length; i++) {
+        items[i].classList.remove("active");
+    }
+    element.classList.add("active");
+}
