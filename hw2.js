931adbcc7224e23e7e175f8d579bccd3813f6767
Bryan Liang <liangbryan2@gmail.com>
2020-01-25 19:11:26 -0800

    hw2 part 2 done

--- hw2.js
+++ hw2.js
@@ -1,6 +1,6 @@
 function changePart(element) {
     let items = document.getElementsByClassName('part');
-    for(var i = 0; i < items.length; i++) {
+    for (var i = 0; i < items.length; i++) {
         items[i].style.display = "none";
     }
     document.getElementById(element).style.display = "block";
@@ -8,8 +8,17 @@ function changePart(element) {
 
 function active(element) {
     let items = document.getElementsByClassName('hw2-nav');
-    for(var i = 0; i < items.length; i++) {
+    for (var i = 0; i < items.length; i++) {
         items[i].classList.remove("active");
     }
     element.classList.add("active");
 }
+
+$(document).ready(function () {
+    $("#fade").click(function () {
+        $("#result").fadeTo("slow", 0.4);
+    })
+    $("#reset").click(function () {
+        $("#result").fadeTo("fast", 1);
+    })
+})
